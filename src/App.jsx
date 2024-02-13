0f8d8cd5b52ebc875af48d8abcb241ef513d730f
Paula Vieira <eipvi@proton.me>
2024-02-13 16:03:26 -0300

    feat: import route
    route home and about

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,29 @@
+import { BrowserRouter, Routes, Route} from 'react-router-dom';
+
+//sass
+import '/src/ui/sass/app.scss';
+
+//pages
+import Home from './ui/pages/Home/Home';
+import About from './ui/pages/About/About';
+
+
 function App() {
 
   return (
-    <>
-     
-    </>
+    <div className='App'>
+     <BrowserRouter>
+    <div className='container'>
+    <Routes>
+      <Route path='/' element={<Home />} />
+      <Route path='/about' element={<About />} />
+    </Routes>
+    </div>
+       </BrowserRouter>
+    </div>
   )
 }
 
 export default App
+
+// , Navigate 
