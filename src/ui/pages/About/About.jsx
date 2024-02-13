68d25721193862bb7dd802674f9350eac9b24978
Paula Vieira <eipvi@proton.me>
2024-02-13 16:04:05 -0300

    feat: componente and import

---
+++ src/ui/pages/About/About.jsx
@@ -0,0 +1,11 @@
+import '/src/ui/sass/app.scss';
+
+const About = () => {
+  return (
+    <div>
+        <h2>about</h2>
+    </div>
+  )
+}
+
+export default About
---
+++ src/ui/pages/Home/Home.jsx
@@ -0,0 +1,11 @@
+import '/src/ui/sass/app.scss';
+
+const Home = () => {
+  return (
+    <div>
+        <h2>Home</h2>
+    </div>
+  )
+}
+
+export default Home
