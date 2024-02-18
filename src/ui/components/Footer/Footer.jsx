583fb23b74073a24f08221d8045284e09025391f
Paula Vieira <eipvi@proton.me>
2024-02-17 23:06:17 -0300

    feat: footer

---
+++ src/ui/components/Footer/Footer.jsx
@@ -0,0 +1,11 @@
+import '/src/ui/sass/app.scss';
+
+const Footer = () => {
+  return (
+<footer className="footer">
+  <p> &copy; 2024 Paula V. Made with ❤️ All rights reserved.</p>
+</footer>  
+);
+};
+
+export default Footer
